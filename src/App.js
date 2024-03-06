98e0452b45b4ee5c783dc28a6a681d768e6f9ed0
Innocentemeka <innocent7932@gmail.com>
2024-03-06 13:57:59 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import LightDarkMode from "./components/lightDarkMode";
+
+
+function App() {
+  return <div className="App">
+   <LightDarkMode/>
+  </div>;
+}
+
+export default App;
